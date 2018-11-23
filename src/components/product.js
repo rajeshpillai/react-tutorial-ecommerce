6cc470240b5148bea020b6bfeb8cae2139166bdc
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-23 07:14:32 +0530

    Modal: Conditionally attach click handler to show modal to avoid closing it accidentally

--- src/components/product.js
+++ src/components/product.js
@@ -13,6 +13,17 @@ export default class Product extends Component {
         });
     }
 
+    showProduct = (product, modal) => {
+        return (
+            <div className="product-item" onClick={modal && this.showModal}>
+                <h3 className="product-title">{product.title}</h3>
+
+                <img src={product.featured_image_url} alt="featured product"></img>
+                <div className="product-desc">{product.description}</div>
+                <button onClick={(e) => { this.props.onAddToCart(product.id) }} className="btn-add-to-cart">Add to Cart</button>
+            </div>
+        );
+    }
     render() {
         let product = this.props.product;
         let productView = (
@@ -26,11 +37,11 @@ export default class Product extends Component {
         );
         return (
             <div >
-                {productView}
+                {this.showProduct(product, true)}
                 {this.state.show && <Modal
                     onClose={this.showModal}
                     show={this.state.show}>
-                    {productView}
+                    {this.showProduct(product, false)}
                 </Modal>
                 }
             </div>
