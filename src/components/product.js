ae91c58fd2f8605eeae8a9c1fe72e1d78ea59d03
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-23 06:52:09 +0530

    Adding simple modal dialog

--- src/components/product.js
+++ src/components/product.js
@@ -1,17 +1,34 @@
 import './products';
 import React, { Component } from 'react';
-
+import Modal from './modal';
 
 export default class Product extends Component {
+    state = {
+        show: false
+    }
+
+    showModal = () => {
+        this.setState({
+            show: !this.state.show
+        });
+    }
+
     render() {
         let product = this.props.product;
         return (
-            <div className="product-item">
-                <h3 className="product-title">{product.title}</h3>
+            <div >
+                <div className="product-item" onClick={this.showModal}>
+                    <h3 className="product-title">{product.title}</h3>
 
-                <img src={product.featured_image_url} alt="featured product"></img>
-                <div className="product-desc">{product.description}</div>
-                <button onClick={(e) => { this.props.onAddToCart(product.id) }} className="btn-add-to-cart">Add to Cart</button>
+                    <img src={product.featured_image_url} alt="featured product"></img>
+                    <div className="product-desc">{product.description}</div>
+                    <button onClick={(e) => { this.props.onAddToCart(product.id) }} className="btn-add-to-cart">Add to Cart</button>
+                </div>
+                <Modal
+                    onClose={this.showModal}
+                    show={this.state.show}>
+                    This message is from Modal!
+                </Modal>
             </div>
         )
     }
